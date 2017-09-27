61379a20e2e117e8c518d299a63aaad1fb5ffe5e
David Mallard <correspondence@davidmallard.id.au>
2017-09-27 20:29:40 +1000

    Adjust indentation to match my editor preferences

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
-// import * as BooksAPI from './BooksAPI'
+//import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-  }
+    state = {
+    }
 
-  render() {
-    return (
-      <div className="app">
-        <Route path='/search' render={() => (
-            <SearchBooks />
-        )}/>
-        <Route exact path="/" render={() => (
-            <ListBooks />
-        )}/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="app">
+                <Route path='/search' render={() => (
+                    <SearchBooks />
+                )}/>
+                <Route exact path="/" render={() => (
+                    <ListBooks />
+                )}/>
+            </div>
+        )
+    }
 }
 
 export default BooksApp
