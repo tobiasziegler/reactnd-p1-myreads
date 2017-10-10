47a6ccb40c161c85eae997ada486bff218ab9ccd
David Mallard <correspondence@davidmallard.id.au>
2017-10-10 18:47:05 +1100

    Display the currently-assigned shelf as the value in the Book's drop-down selector

--- src/Book.js
+++ src/Book.js
@@ -14,7 +14,9 @@ class Book extends Component {
 				<div className="book-top">
 					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
 					<div className="book-shelf-changer">
-						<select>
+						<select
+							value={book.shelf}
+						>
 							<option value="none" disabled>Move to...</option>
 							<option value="currentlyReading">Currently Reading</option>
 							<option value="wantToRead">Want to Read</option>
