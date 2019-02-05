3996afb3070001d3faae8fae74f73a32cb4ce584
David Mallard <correspondence@davidmallard.id.au>
2019-02-05 13:28:58 +1100

    Updates

---
+++ precache-manifest.bb5a3ccd766930a4447149a99e12372f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aee082be4bf645ec3e24",
+    "url": "/reactnd-p1-myreads/static/css/main.a94c15ea.chunk.css"
+  },
+  {
+    "revision": "aee082be4bf645ec3e24",
+    "url": "/reactnd-p1-myreads/static/js/main.aee082be.chunk.js"
+  },
+  {
+    "revision": "e391f0ffea3e4e5dcb36",
+    "url": "/reactnd-p1-myreads/static/js/1.e391f0ff.chunk.js"
+  },
+  {
+    "revision": "8f1a1d3777d0f509677f",
+    "url": "/reactnd-p1-myreads/static/js/runtime~main.8f1a1d37.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-p1-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-p1-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-p1-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "74c8cb80ef7b8041a6b27a23ec63b18f",
+    "url": "/reactnd-p1-myreads/index.html"
+  }
+];
